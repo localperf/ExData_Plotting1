7a07e21bfebfa2f83f7523de43dbba1898ed72e1
localperf <gerard.blais@gmail.com>
2014-07-07 10:36:34 -0400

    updated plot4.r to save graph as png

--- plot4.r
+++ plot4.r
@@ -23,3 +23,30 @@ summary(data)
 setwd("c://coursera//exploratory//assignment1")
 
 #--plot4
+
+par (mfrow = c(1, 2))
+
+
+plot  (data$ts, data$Sub_metering_1, type = "s", col = "black", xlab = "", ylab = "Energy sub-metering")
+lines (data$ts, data$Sub_metering_2, type = "s", col = "red")
+lines (data$ts, data$Sub_metering_3, type = "s", col = "blue")
+legend ("topright", c("Sub_Metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1,
+        col = c("black", "red", "blue"))
+
+plot (data$ts, data$Global_reactive_power, type = "s", xlab = "datetime", ylab = "Global_reactive_power")
+
+
+
+
+png ("plot4.png", h = 480, w = 480)
+par (mfrow = c(1, 2))
+
+
+plot  (data$ts, data$Sub_metering_1, type = "s", col = "black", xlab = "", ylab = "Energy sub-metering")
+lines (data$ts, data$Sub_metering_2, type = "s", col = "red")
+lines (data$ts, data$Sub_metering_3, type = "s", col = "blue")
+legend ("topright", c("Sub_Metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1,
+        col = c("black", "red", "blue"))
+
+plot (data$ts, data$Global_reactive_power, type = "s", xlab = "datetime", ylab = "Global_reactive_power")
+dev.off()
